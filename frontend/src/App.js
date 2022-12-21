9edbe431eb5619ca1d43a8d01a914f379c004f43
lindanorberg <linda@norberg.tv>
2022-12-21 16:09:58 +0100

    add inputPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,34 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+
+import { user } from './reducers/user';
+
+import { LoginPage } from './pages/LoginPage';
+import { InputPage } from './pages/InputPage';
+// import { CalendarViewPage } from './pages/CalendarViewPage'
+// import { NotFoundPage } from './pages/NotFoundPage'
 
 const reducer = combineReducers({
-    user: user.reducer,
+  user: user.reducer
 });
 
 const store = configureStore({ reducer });
 
 export const App = () => {
-    return(
-        <Provider store={store}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<LoginPage />} />
-                    <Route path="/input" element={<InputPage />} />
-                    <Route path="/calender" element={<CaleandarView/>} />
-                    <Route path="/about" element={<About/>} />
-                    <Route path="/*" element={<NotFound/>} />
-                </Routes>
-            </BrowserRouter>
-        </Provider>
-    )
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/input" element={<InputPage />} />
+          {/* <Route path="/calender" element={<CalendarViewPage />} />
+          <Route path="/*" element={<NotFoundPage />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  )
 }
 
+// <Route path="/about" element={<About />} />
---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector, batch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import { API_URL } from '../utils/urls';
+import { user } from '../reducers/user';
+
+export const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const accessToken = useSelector((store) => store.user.accessToken);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+
+  // if there is an accessToken the user will be directed to the inputPage. 
+  useEffect(() => {
+    if (accessToken) {
+      navigate('/input');
+    }
+  }, [accessToken, navigate]);
+
+  const onFormSubmit = (event) => {
+    console.log(accessToken)
+    event.preventDefault();
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ 
+        username: username,
+        password: password 
+      })
+    };
+
+    fetch(API_URL('login'), options)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          batch(() => {
+            dispatch(user.actions.setUserId(data.response.userId));
+            dispatch(user.actions.setUsername(data.response.username));
+            dispatch(user.actions.setAccessToken(data.response.accessToken));
+            dispatch(user.actions.setError(null));
+            // navigate("/input");
+          })
+        } else {
+        // setErrorMessage(data.response);
+          batch(() => {
+            dispatch(user.actions.setUserId(null));
+            dispatch(user.actions.setUsername(null));
+            dispatch(user.actions.setAccessToken(null));
+            dispatch(user.actions.setError(data.response));
+            console.log(data.response);
+          });
+        }
+      });
+  };
+  return (
+    <>
+      <h1>Please log in</h1>
+      <form onSubmit={onFormSubmit}>
+        <label htmlFor="username">Username
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)} />
+        </label>
+        <label htmlFor="password">Password
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  )
+}
+
