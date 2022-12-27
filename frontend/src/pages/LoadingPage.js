3d803816e38d2f37152162506d13fd848f8fd53b
Sofia Andersson <sofia.e.i.andersson@gmail.com>
2022-12-27 15:54:08 +0100

    Button component added. loading component started

---
+++ frontend/src/pages/LoadingPage.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Lottie from 'lottie-react';
+import styled from 'styled-components'
+import LoadingDot from 'lotties/loading';
+
+// Loading component rendered when isLoading=true
+export const LoadingPage = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: LoadingDot,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
+  // Lottie-animation from json-file showing one growing dot when loading
+  return (
+    <LoadingOuterWrapper>
+      <LoadingPageWrapper>
+        <Lottie animationData={LoadingDot} options={defaultOptions} height={400} width={400} />
+      </LoadingPageWrapper>
+    </LoadingOuterWrapper>
+  );
+}
+
+const LoadingOuterWrapper = styled.div`
+position: relative;
+width: 100%;
+display: flex;
+justify-content: center;
+align-items: center;
+`;
+
+const LoadingPageWrapper = styled.div`
+  background: rgba(245, 245, 245, 0.541);
+  position: absolute;
+  z-index: 1;
+  width: 250px;
+  min-height: 360px;
+  margin-bottom: -50px;
+  margin-top: 30px;
+  top: 8px;
+  padding: 30px;
+  border-radius: 24px;
+
+  @media (min-width: 668px){
+  margin-top: 10%;
+  }
+`
